6abf69f752fae13f1829207b8d830137fc492b53
dieheartfanofalluarjun <107054018+dieheartfanofalluarjun@users.noreply.github.com>
2022-07-08 17:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
 
+vaishuu=Client(
+       "vaishali x music new",
+       api_hash="",
+       api_id="",
+       bot_token="", 
+) 
+
+@vaishuu.on_message(filters.command("start"))
+async def start(bot, message):
+       await message.reply_text("Hello user iam a official pyrogram music bot")
+    
+@vaishuu.on_message(filters.command("help"))
+async def help(bot, message):
+       await message.reply_text("hello user this is my help buttons click help")
+
+
+vaishuu.run() 
